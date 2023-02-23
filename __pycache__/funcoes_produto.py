01c87e5d2118179108092bb576e03dc311882a65
ManuelaRiosSilva <manuela.silva@contmatic.com.br>
2023-02-23 13:45:45 -0300

    ajuste das funcoes de documentos

--- __pycache__/funcoes_produto.py
+++ __pycache__/funcoes_produto.py
@@ -12,7 +12,7 @@ fake = Faker(locales)
 status = ['Ativo', 'Inativo']
 origem = ['0 - Nacional: exceto as indicadas nos códigos 3, 4, 5 e 8', '1 - Estrangeira: importação direta, exceto a indicada no código 6', '2 - Estrangeira: adquirida no mercado interno, exceto a indicada no código 7', '3 - Nacional: mercadoria ou bem com Conteúdo de Importação superior a 40% e inferior ou igual a 70%', '4 - Nacional: produção em conformidade com processos básicos tratados na legislação dos Ajustes', '5 - Nacional: mercadoria ou bem com Conteúdo de Importação inferior ou igual a 40%', '6 - Estrangeira: importação direta, sem similar nacional, constante em lista da CAMEX', '7 - Estrangeira: adquirida no mercado interno, sem similar nacional, constante em lista da CAMEX', '8 - Nacional: mercadoria ou bem com Conteúdo de Importação superior a 70%']
 tipoProduto = ['00 - Mercadoria para Revenda', '01 - Matéria Prima', '02 - Embalagens', '03 - Produto em Processo', '04 - Produto Acabado', '05 - Subproduto', '06 - Produto Intermediário', '07 - Material de uso e consumo', '08 - Ativo Imobilizado', '10 - Outros Insumos', '99 - Outros']
-unidadeMedida = ['Ampola','Balde','Barra','Bobina','Bloco']
+unidadeMedida = ['BALDE', 'BANDEJA', 'BOMB', 'CAPS', 'CART', 'CENTO', 'CJ', 'CM', 'LATA']
 ncm = ['01012100','02011000', '12116000', '55094200', '90302010']
 cest = ['0100100', '0200100', '0300100', '0400100', '2899900']
 
@@ -76,7 +76,6 @@ def generate_aliquotaTributosEstadual():
 
 def generate_CEST():
     return random.choice(cest)
-
 
 def generate_codigoTributacao():
     return fake.pyint()
---
+++ __pycache__/listaValoresPreenchimento.py
@@ -0,0 +1,18 @@
+#Valores preenchimento pré-definidos
+status = ['Ativo']
+tipoCadastro = ['Cliente', 'Fornecedor', 'Transportadora', 'Cliente | Fornecedor', 'Cliente | Fornecedor | Transportadora', 'Cliente | Transportadora', 'Fornecedor | Transportadora']
+perfilParceiro = ['Pessoa Física', 'Pessoa Jurídica', 'Estrangeiro']
+tipoInscricaoEstadual = ['Isento', 'Contribuinte', 'Não contribuinte']
+cnae = ['3250706','4530702','4530706','4613300']
+suframa = ['82252918', '87866990', '24794329']
+tipoTelefone = ['Celular', 'Comercial', 'Residencial', 'FAX', 'Outros']
+tipoEndereco = ["COMERCIAL", "RESIDENCIAL", "COBRANCA", "ENTREGA", "OUTROS"]
+
+#Campos Planilha
+#camposValidadores =  ["Perfil do parceiro", "Tipo de Inscrição Estadual"] 
+
+campoValidadorPerfilParceiro = "Perfil do parceiro"
+camposValidarPerfilParceiro = ["Documento de Identificação","Razão social/nome","Nome Fantasia","Data de aniversário","CNAE Principal", "CPF/CNPJ contato"]
+
+campoValidadorTipoInscricao = "Tipo de Inscrição Estadual"
+camposValidarInscricao = ["Inscrição Estadual","SUFRAMA"]
